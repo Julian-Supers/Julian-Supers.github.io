9dd1dd341f22e11483c65043efeaf5eaa7a62f98
Julian-Supers <julian.supers@dcmail.ca>
2025-03-04 12:24:02 -0500

    Update ball data
    in this commit i added an update function for the ball

--- assignment-4-part-3/main.js
+++ assignment-4-part-3/main.js
@@ -40,9 +40,23 @@ draw() {
   ctx.fill();
 }
 
+update() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
 
-const testBall = new Ball(50, 100, 4, 4, "blue", 10);
-testBall.x;
-testBall.size;
-testBall.color;
-testBall.draw();
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}

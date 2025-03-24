7e1ac29655a952102e49916ba6189c3058bbac49
Julian-Supers <julian.supers@dcmail.ca>
2025-03-24 17:56:24 -0400

    update

--- assignmetn-4-part-4/main-finished.js
+++ assignmetn-4-part-4/main-finished.js
@@ -71,6 +71,55 @@ class Ball {
   }
 }
 
+class evilcircle extends shape{
+  constructor(x,y) {
+    super(x,y,20,20)
+    this.color="white"
+    this.size=10
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.lineWidth=3;
+  }
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.x = this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.x = this.size;
+    }
+
+    if ((this.y - this.size) >= height) {
+      this.y = this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y = this.size;
+    }
+  }
+}
+
 const balls = [];
 
 while (balls.length < 25) {

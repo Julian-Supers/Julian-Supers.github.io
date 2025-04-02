41c23afe0717a788311993525a5e32f214b122ab
Julian-Supers <julian.supers@dcmail.ca>
2025-04-01 21:43:07 -0400

    Update main.js
    in this commit i added comments

--- assignment-4-part-1/main.js
+++ assignment-4-part-1/main.js
@@ -1,7 +1,9 @@
+/* In the raw text file, copy all of the code underneath the heading "1. 
+COMPLETE VARIABLE AND FUNCTION DEFINITIONS" and paste it into the top of the main.js file. */
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
-
+/* Declaring function that gets a random value from the array */ 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
@@ -12,11 +14,13 @@ const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
+/* This event listener waits for a click event before it starts the result */
 randomize.addEventListener('click', result);
-
+/* Declares a function that */
 function result() {
     let newStory = storyText;
-
+    /* Create three new variables called xItem, yItem, and zItem, 
+    and make them equal to the result of calling randomValueFromArray() on your three arrays */
     const xItem = randomValueFromArray(insertX);
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
@@ -27,9 +31,10 @@ function result() {
 
     if(customName.value !== '') {
         const name = customName.value;
+        /* Inside the first if block, add another string replacement method call to replace the name 'Bob' found in the newStory string with the name variable. */
         newStory = newStory.replaceAll('Bob', name);
     }
-
+    /* checking to see if the uk radio button has been selected. If so, convert the weight and temperature values in the story from pounds and Fahrenheit into stones and centigrade. */
     if(document.getElementById("uk").checked) {
         const weight = `${Math.round(300*0.0714286)} stone`;
         const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
